2b42e5c96845edf8f7e5ac26d0fde0a162fd0c2f
Vlas <vlasjack@gmail.com>
2022-11-23 23:03:14 +0300

    Commit 2

--- src/CheckA.java
+++ src/CheckA.java
@@ -1,13 +1,38 @@
-import java.io.File;
+import java.io.IOException;
+import java.lang.String;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class CheckA {
-    public String CheckerA() {
-        File file = new File("C:/ProgramData/Microsoft/Windows Defender/Platform/4.18.2210.6-0/MsMpEng.exe");
 
-        String res=null;
+    public String CheckerA()
+    {
 
-        if  (file.exists())
-            res="Антивирус установлен";
-        else  res="Антивирус  не установлен";
-        return res;
+        String check = new String("").intern();
+        String loc = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows Defender";
+        String name = "DisableAntivirus";
+        String regType = "REG_SZ";
+        try {
+            Process p = Runtime.getRuntime().exec("reg QUERY \"" + loc + "\" /v \"" + name + "\"");
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String rees = "";
+            while ((rees = in.readLine()) != null) {
+                if (rees.matches("(.*)\\s+REG_(.*)")) {
+                    break;
+                }
+            }
+            in.close();
+            String res;
+            if (rees != null) {
+                int indexOf = rees.indexOf(regType);
+                if (indexOf != 0) {
+               
+                    return res = "Антивирус установлен";
+                }
+            }
+            return  res = "Антивирус не установлен";
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
